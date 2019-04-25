3af740fe776f8ff951b672d25b62c959be4135e4
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2019-04-25 02:24:29 -0500

    added postProcessing app

---
+++ app/src/main/java/com/example/vishn/postflightprocessing/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.vishn.postflightprocessing;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+
+
+
+}
